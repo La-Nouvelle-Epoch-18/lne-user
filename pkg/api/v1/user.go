996360381b03ebcd5d4b5a3b329897d0c5f8d596
amine <hilalyamine@gmail.com>
2019-12-06 04:45:06 +0100

    tset

--- pkg/api/v1/user.go
+++ pkg/api/v1/user.go
@@ -75,7 +75,25 @@ func (a *Api) HandleGetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	setupResponse(&w, req)
+	if (*req).Method == "OPTIONS" {
+		return
+	}
+}
+
 func (a *Api) HandleGetReadme(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+
 	resp, err := http.Get("https://api.github.com/repos/La-Nouvelle-Epoch-18/Ine-front/readme")
 	if err != nil {
 		w.WriteHeader(http.StatusOK)
